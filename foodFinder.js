40fd092afd592adc070c62797f13d99918004bd2
BlazejW <widera.blazej@onet.pl>
2021-12-05 01:05:18 +0100

    Updated and working API

--- foodFinder.js
+++ foodFinder.js
@@ -25,20 +25,21 @@ function getMealList(){
                 let rndInt = randomIntFromInterval(35, 99);
                 html += `
                     <div class="proposition" data-id = "${meal.idMeal}">
-                    <div class="food-img">
-                        <img src="${meal.strMealThumb}" alt="pasta img">
-                    </div>
-                    <div class="food-content">
-                        <div class="food-name">
-                            <h3>${meal.strMeal}</h3>
+                        <div class="food-img">
+                            <img src="${meal.strMealThumb}" alt="pasta img">
                         </div>
-                        <div class="food-read">
-                            <p class="food-read--p">Read more</p>
-                            <h3 class="slide-food slide-food--name">${meal.strMeal}</h3>
-                            <div class="slide-food slide-food--description">
-                                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tempore, aspernatur odit tempora enim minima quod a nemo culpa. Iste accusamus ratione excepturi, officiis ipsa magni architecto qui veniam ex voluptates!</p>
+                        <div class="food-content">
+                            <div class="food-name">
+                                <h3>${meal.strMeal}</h3>
                             </div>
-                            <p class="slide-food slide-food--price">${rndInt},-</p>
+                            <div class="food-read">
+                                <p class="food-read--p">Read more</p>
+                                <h3 class="slide-food slide-food--name">${meal.strMeal}</h3>
+                                <div class="slide-food slide-food--description">
+                                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tempore, aspernatur odit tempora enim minima quod a nemo culpa. Iste accusamus ratione excepturi, officiis ipsa magni architecto qui veniam ex voluptates!</p>
+                                </div>
+                                <p class="slide-food slide-food--price">${rndInt},-</p>
+                            </div>
                         </div>
                     </div>
                 `;
